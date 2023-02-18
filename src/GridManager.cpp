6502a179137d3f149f4376b311059fa6f2b72bc4
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-02-18 17:40:47 -0300

    refactor: refactored and added doc for GridManager class

--- src/GridManager.cpp
+++ src/GridManager.cpp
@@ -2,26 +2,18 @@
 #include <iostream>
 #include <iomanip>
 
+const int START_X = 0;
+const int START_Y = 0;
+const int EXTRA_GRID_SPACE = 3;
+
 GridManager::GridManager()
 {
-    columns = 0;
     rows = 0;
     width = 0;
     height = 0;
+    columns = 0;
     tileSize = 0;
     grid = nullptr;
-}
-
-void GridManager::updateGridSize(int windowWidth, int windowHeight)
-{
-    clearGrid();
-    width = windowWidth;
-    height = windowHeight;
-
-    const int EXTRA_SPACE = 3;
-    columns = (width / tileSize) + EXTRA_SPACE;
-    rows = (height / (tileSize / 4)) + EXTRA_SPACE;
-    generateGrid();
 }
 
 void GridManager::initialize(int windowWidth, int windowHeight, int tileSize)
@@ -32,6 +24,20 @@ void GridManager::initialize(int windowWidth, int windowHeight, int tileSize)
     updateGridSize(width, height);
 }
 
+void GridManager::updateGridSize(int windowWidth, int windowHeight)
+{
+    // Important: We need to clear the grid before changing the width and height
+    // of the grid. Otherwise, we will have a memory leak.
+    clearGrid();
+    width = windowWidth;
+    height = windowHeight;
+    columns = (width / tileSize) + EXTRA_GRID_SPACE;
+    rows = (height / (tileSize / 4)) + EXTRA_GRID_SPACE;
+    generateGrid();
+    populateGrid(START_X, START_Y);
+    printGrid();
+}
+
 void GridManager::updateTileSize(int tileSize)
 {
     this->tileSize = tileSize;
@@ -40,76 +46,83 @@ void GridManager::updateTileSize(int tileSize)
 
 void GridManager::generateGrid()
 {
-    // Create the new grid
     grid = new int **[rows];
-
-    for (int i = 0; i < rows; i++)
+    for (int row = 0; row < rows; row++)
     {
-        grid[i] = new int *[columns];
-        for (int j = 0; j < columns; j++)
-        {
-            if (i % 2 == 0 && j == columns - 1)
-                continue; // Skip the last column of even lines (it's not needed)
-            grid[i][j] = new int[2];
-        }
-    }
-
-    // Populate the grid
-    int startX = 0;
-    int startY = 0;
-
-    for (int i = 0; i < rows; i++)
-    {
-        // Calculate the start point
-        if (i != 0)
-        {
-            if (i % 2 == 0) // Even
-            {
-                startX = grid[i - 1][0][0];
-                startY = grid[i - 1][0][1] + 1;
-            }
-            else
-            {
-                startX = grid[i - 1][0][0] + 1;
-                startY = grid[i - 1][0][1];
-            }
-        }
-
-        grid[i][0][0] = startX;
-        grid[i][0][1] = startY;
-
-        for (int j = 0; j < columns; j++)
-        {
-            if (i % 2 == 0 && j == columns - 1)
-                continue; // Skip the last column of even lines (it's not needed)
-            grid[i][j][0] = startX - j;
-            grid[i][j][1] = startY + j;
-        }
-    }
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            if (i % 2 == 0 && j == columns - 1)
-                continue; // Skip the last column of even lines (it's not needed)
-            std::cout << "[" << std::setw(3) << grid[i][j][0] << "," << std::setw(3) << grid[i][j][1] << "] ";
-        }
-        std::cout << std::endl;
+        grid[row] = new int *[columns];
+        for (int column = 0; column < columns; column++)
+            if (!shouldSkipTile(row, column))
+                grid[row][column] = new int[2];
     }
 }
 
 void GridManager::clearGrid()
 {
+    for (int row = 0; row < rows; row++)
+    {
+        for (int column = 0; column < columns; column++)
+            if (!shouldSkipTile(row, column))
+                delete[] grid[row][column];
+        delete[] grid[row];
+    }
+    delete[] grid;
+}
+
+void GridManager::populateGrid(int startX, int startY)
+{
+    // Holds the start coordinates of the current row
+    // This will be used to calculate the position of each tile in the row
+    int currentRowStartX = startX;
+    int currentRowStartY = startY;
+
+    for (int row = 0; row < rows; row++)
+    {
+        // For each row (except the first one) we need to calculate the start
+        // coordinates before proceeding to the tile's coordinates calculation.
+        // The start coordinates are calculated based on the previous row start coordinates.
+        // For even rows, the X keeps the same value, while the Y is incremented by 1.
+        // For odd rows, the X is incremented by 1, while the Y keeps the same value.
+        if (row != 0)
+        {
+            if (row % 2 == 0)
+            {
+                startX = grid[row - 1][0][0];
+                startY = grid[row - 1][0][1] + 1;
+            }
+            else
+            {
+                startX = grid[row - 1][0][0] + 1;
+                startY = grid[row - 1][0][1];
+            }
+        }
+
+        // To calculate the coordinates of each tile in the current row we should
+        // start from the start coordinates of the current row and then proceed
+        // to the next tile by decrementing the X and incrementing the Y by it's
+        // position on the current row (the column index).
+        for (int column = 0; column < columns; column++)
+        {
+            if (!shouldSkipTile(row, column))
+            {
+                grid[row][column][0] = startX - column;
+                grid[row][column][1] = startY + column;
+            }
+        }
+    }
+}
+
+void GridManager::printGrid()
+{
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
-        {
-            if (i % 2 == 0 && j == columns - 1)
-                continue; // Skip the last column of even lines (it's not needed)
-            delete[] grid[i][j];
-        }
-        delete[] grid[i];
+            if (!shouldSkipTile(i, j))
+                std::cout << "[" << std::setw(3) << grid[i][j][0] << "," << std::setw(3) << grid[i][j][1] << "] ";
+        std::cout << std::endl;
     }
-    delete[] grid;
+}
+
+bool GridManager::shouldSkipTile(int currentLine, int currentColumn)
+{
+    return currentLine % 2 == 0 && currentColumn == columns - 1;
 }
